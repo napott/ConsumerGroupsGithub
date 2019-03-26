9ad31ebc714551186b49f2528686912092501266
Nathaniel Potter <napott@microsoft.com>
2019-03-26 16:40:30 -0700

    Fix url

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ module.exports = app => {
     let githubEvent = new GithubEvent({
       eventType: 'issue',
       state: context.payload.issue.state,
-      url: context.payload.issue.url,
+      url: context.payload.issue.html_url,
       repo: context.payload.repository.id,
       title: context.payload.issue.title,
       body: context.payload.issue.body,
@@ -108,7 +108,7 @@ module.exports = app => {
     let query = {githubId: context.payload.issue.id};
     let update = {
       state: context.payload.issue.state,
-      url: context.payload.issue.url,
+      url: context.payload.issue.html_url,
       title: context.payload.issue.title,
       body: context.payload.issue.body,
     }
@@ -125,7 +125,7 @@ module.exports = app => {
     let githubEvent = new GithubEvent({
       eventType: 'pull_request',
       state: context.payload.pull_request.state,
-      url: context.payload.pull_request.url,
+      url: context.payload.pull_request.html_url,
       repo: context.payload.repository.id,
       title: context.payload.pull_request.title,
       body: context.payload.pull_request.body,
@@ -140,7 +140,7 @@ module.exports = app => {
     let query = {githubId: context.payload.pull_request.id};
     let update = {
       state: context.payload.pull_request.state,
-      url: context.payload.pull_request.url,
+      url: context.payload.pull_request.html_url,
       title: context.payload.pull_request.title,
       body: context.payload.pull_request.body,
     }
@@ -156,7 +156,6 @@ module.exports = app => {
   app.on('push', async context => {
     let githubEvent = new GithubEvent({
       eventType: 'push',
-      state: 'closed',
       repo: context.payload.repository.id,
       url: context.payload.compare,
     });
