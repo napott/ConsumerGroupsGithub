4d3b8927b636cc331f2c6ae078e99f01752d2a4c
Nate Potter <ntpttr@outlook.com>
2019-03-26 18:55:33 -0700

    Break router into separate file

--- index.js
+++ index.js
@@ -5,13 +5,9 @@
 module.exports = app => {
   require('dotenv').config();
 
-  const express = require('express');
-  const bodyParser = require('body-parser');
   const mongoose = require('mongoose');
 
   const GithubEvent = require('./schemas/githubEvent');
-  const Group = require('./schemas/groupSchema');
-  const consumerGroup = require('./consumerGroup')
 
   // ------------------------------ DB and router setup ------------------------------
 
@@ -26,98 +22,7 @@ module.exports = app => {
   .then(() => console.log('Connection to CosmosDB successful'))
   .catch((err) => console.error(err));
 
-  // Create test end point for consumer groups apis
-  const router = app.route('/groups');
-  router.use(require('express').static('public'));
-
-  router.use(bodyParser.urlencoded({extended: false}));
-  router.use(bodyParser.json());
-
-  router.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-
-  // ------------------------------ App API routes ------------------------------
-
-  /**
-   * Get the repo events for a given group
-   */
-  router.get('/events', (req, res) => {
-    const groupAddress = req.query.groupAddress;
-    Group.findOne({address: groupAddress}, function(err, group) {
-      if (err) {
-        res.status(500).json({'message': 'Error finding group'});
-        return;
-      } else if (group == null) {
-        res.status(404).json({'message': 'Group to gather events for not found'});
-        return;
-      } else {
-        GithubEvent.find({repo: {$in: group.repos}}, function(err, events) {
-          if (err) {
-            res.status(500).json({'message': 'There was an error retrieving the events'});
-          } else {
-            res.status(200).json({events: events});
-          }
-        })
-      }
-    })
-  });
-
-  /**
-   * Add a repo to a group's list of connected repos
-   */
-  router.post('/repos', (req, res) => {
-    const groupAddress = req.body.groupAddress;
-    const repo = req.body.repo;
-
-    if (groupAddress == null || repo == null) {
-      res.status(400).json({'message': 'Must include group address and repo'});
-      return;
-    }
-    
-    let conditions = {
-      address: groupAddress,
-    };
-
-    let update = {
-      $addToSet: { repos: repo },
-    };
-
-    Group.findOneAndUpdate(conditions, update, {upsert: true}, function(err, group) {
-      if (err) {
-        res.status(500).json({'message': 'There was an error saving the group repos'});
-      } else {
-        res.status(200).json({'message': 'Group repo added'});
-      }
-    });
-  });
-
-  /**
-   * Send an email to a group
-   */
-  router.post('/email', (req, res) => {
-    const groupAddress = req.body.groupAddress;
-    const subject = req.body.subject;
-    const emailBody = req.body.emailBody;
-
-    consumerGroup.writeSimpleEmail(groupAddress, subject, emailBody, function(result) {
-      res.status(200).json({'result': result});
-    });
-  });
-
-  /**
-   * Add a member to a consumer group
-   */
-  router.post('/members', (req, res) => {
-    const groupAddress = req.body.groupAddress;
-    const userAddress = req.body.userAddress;
-
-    consumerGroup.addMemberToConsumerGroup(groupAddress, userAddress, function(result) {
-      res.status(200).json({'result': result});
-    })
-  });
+  require('./router')(app);
 
   // ------------------------------ Github event handlers ------------------------------
 
---
+++ router.js
@@ -0,0 +1,100 @@
+const bodyParser = require('body-parser');
+
+const Group = require('./schemas/groupSchema');
+const GithubEvent = require('./schemas/githubEvent');
+const consumerGroup = require('./consumerGroup')
+
+module.exports = app => {
+    // ------------------------------ Router Setup ------------------------------
+    const router = app.route('/groups');
+    router.use(require('express').static('public'));
+
+    router.use(bodyParser.urlencoded({extended: false}));
+    router.use(bodyParser.json());
+
+    router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+    });
+
+    // ------------------------------ App API routes ------------------------------
+
+    /**
+     * Get the repo events for a given group
+     */
+    router.get('/events', (req, res) => {
+    const groupAddress = req.query.groupAddress;
+    Group.findOne({address: groupAddress}, function(err, group) {
+        if (err) {
+        res.status(500).json({'message': 'Error finding group'});
+        return;
+        } else if (group == null) {
+        res.status(404).json({'message': 'Group to gather events for not found'});
+        return;
+        } else {
+        GithubEvent.find({repo: {$in: group.repos}}, function(err, events) {
+            if (err) {
+            res.status(500).json({'message': 'There was an error retrieving the events'});
+            } else {
+            res.status(200).json({events: events});
+            }
+        })
+        }
+    })
+    });
+
+    /**
+     * Add a repo to a group's list of connected repos
+     */
+    router.post('/repos', (req, res) => {
+    const groupAddress = req.body.groupAddress;
+    const repo = req.body.repo;
+
+    if (groupAddress == null || repo == null) {
+        res.status(400).json({'message': 'Must include group address and repo'});
+        return;
+    }
+    
+    let conditions = {
+        address: groupAddress,
+    };
+
+    let update = {
+        $addToSet: { repos: repo },
+    };
+
+    Group.findOneAndUpdate(conditions, update, {upsert: true}, function(err, group) {
+        if (err) {
+        res.status(500).json({'message': 'There was an error saving the group repos'});
+        } else {
+        res.status(200).json({'message': 'Group repo added'});
+        }
+    });
+    });
+
+    /**
+     * Send an email to a group
+     */
+    router.post('/email', (req, res) => {
+    const groupAddress = req.body.groupAddress;
+    const subject = req.body.subject;
+    const emailBody = req.body.emailBody;
+
+    consumerGroup.writeSimpleEmail(groupAddress, subject, emailBody, function(result) {
+        res.status(200).json({'result': result});
+    });
+    });
+
+    /**
+     * Add a member to a consumer group
+     */
+    router.post('/members', (req, res) => {
+    const groupAddress = req.body.groupAddress;
+    const userAddress = req.body.userAddress;
+
+    consumerGroup.addMemberToConsumerGroup(groupAddress, userAddress, function(result) {
+        res.status(200).json({'result': result});
+    })
+    });
+}
