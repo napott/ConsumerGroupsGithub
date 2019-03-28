90ad25d9078117ca5460ee713298dea7d0660c08
Juan Ochoa <juanoc@microsoft.com>
2019-03-28 16:11:42 -0700

    something does not work

--- router.js
+++ router.js
@@ -21,18 +21,27 @@ module.exports = app => {
     })
 
     rootRouter.get('/start', (req, res) => {
+
         var groupSmtpAddress = req.query.groupSmtpAddress;
 
+        console.log("start/: ", groupSmtpAddress);
+        res.status(404).json();
         if (groupSmtpAddress)
         {
+            console.log("start2/: ", groupSmtpAddress);
             var keys = [ process.env.COOKIE_KEYS ];
             var cookies = new Cookies(req, res, { keys: keys })
 
+            console.log("start3/: ", groupSmtpAddress);
+            console.log("groupSmtpAddress: ", groupSmtpAddress);
             cookies.set('groupSmtpAddress', groupSmtpAddress, { signed: true });
+
+            console.log("start4/: ", groupSmtpAddress);
             res.redirect(301, "https://github.com/login/oauth/authorize?client_id="+ process.env.GITHUB_CLIENTID +"&redirect_uri="+process.env.APP_ROOT_URL+"/configure&state=12345");
         }
         else
         {
+            console.log("start5/: ", groupSmtpAddress);
             res.status(404).json(); 
         }
 
@@ -137,6 +146,13 @@ module.exports = app => {
                 'Authorization' : 'bearer ' + access_token,
                 'User-Agent': process.env.GITHUB_USERAGENT
             };
+
+            var keys = [ process.env.COOKIE_KEYS ];
+            var cookies = new Cookies(req, res, { keys: keys })            
+            var groupSmtpAddress = cookies.get('groupSmtpAddress', {signed: true});
+
+            console.log("GroupSmtpAddress: ", groupSmtpAddress)
+
             githubController.issue_request(
                 "GET",
                 "https://api.github.com/user/repos?type=owner",
@@ -147,7 +163,8 @@ module.exports = app => {
                     result = JSON.parse(body);
                     res.render('../../../views/selectRepos.hbs', {
                         actionUrl : '/processRepos',
-                        repositories : result
+                        repositories : result,
+                        groupSmtpAddress : 'groupSmtpAddress'
                     });
                 });
         }
@@ -175,7 +192,10 @@ module.exports = app => {
             console.log("The group smtp address: ", groupSmtpAddress);
 
             databaseController.addReposToGroup(groupSmtpAddress, result.repository, function(status, body) {
-                res.status(status).json(body);
+                res.render('../../../views/reposConfigured', {
+                    groupSmtpAddress : groupSmtpAddress,
+                    result : body.message
+                });
             });
         });
     });    
